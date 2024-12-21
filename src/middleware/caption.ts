9c038862f040cc305572ba7c524b71eab2054176
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-12-21 17:54:10 +0900

    feat: 인스타그램 캡션을 검증하는 caption.ts 추가

---
+++ src/middleware/caption.ts
@@ -0,0 +1,42 @@
+const INSTAGRAM_MAX_CAPTION_LENGTH = 2200;
+
+const ERROR_MESSAGES = {
+    EMPTY: '캡션이 비어있습니다.',
+    NOT_STRING: '캡션은 문자열이어야 합니다.',
+    TOO_LONG: `캡션은 ${INSTAGRAM_MAX_CAPTION_LENGTH}자를 넘을 수 없습니다.`,
+    TOO_MANY_HASHTAGS: 'Caption contains more than 30 hashtags',
+} as const;
+
+export function vaildateCaption(caption: string): boolean {
+    captionIsNotEmpty(caption);
+    captionIsString(caption);
+    captionIsNotTooLong(caption);
+    captionHasNotTooManyHashtags(caption);
+
+    return true;
+}
+
+function captionIsNotEmpty(caption: string): void {
+    if(!caption) {
+        throw new Error(ERROR_MESSAGES.EMPTY);
+    }
+}
+
+function captionIsString(caption: string): void {
+    if(typeof caption !== 'string') {
+        throw new Error(ERROR_MESSAGES.NOT_STRING);
+    }
+}
+
+function captionIsNotTooLong(caption: string): void {
+    if(caption.length > INSTAGRAM_MAX_CAPTION_LENGTH) {
+        throw new Error(ERROR_MESSAGES.TOO_LONG);
+    }
+}
+
+function captionHasNotTooManyHashtags(caption: string): void {
+    const hashtagCount = (caption.match(/#/g) || []).length;
+    if (hashtagCount > 30) {
+        throw new Error(ERROR_MESSAGES.TOO_MANY_HASHTAGS);
+    }
+}
