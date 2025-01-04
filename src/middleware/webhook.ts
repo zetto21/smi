135ee6046c69f074e528d5db88bcefd6b97a55fb
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-05 05:04:41 +0900

    chore: 오타 수정

--- src/middleware/webhook.ts
+++ src/middleware/webhook.ts
@@ -1,7 +1,8 @@
-export function vaildateWebhookUrl(webhookUrl: string) {
-    const discordWebhookUrlRegex = /https:\/\/discord.com\/api\/webhooks\/\d+\/[\w-]+/;
+export function validateDiscordWebhookUrl(webhookUrl: string) {
+  const discordWebhookUrlRegex =
+    /https:\/\/discord.com\/api\/webhooks\/\d+\/[\w-]+/;
 
-    if (!discordWebhookUrlRegex.test(webhookUrl)) {
-        throw new Error('올바른 Discord Webhook URL이 아닙니다.');
-    }
+  if (!discordWebhookUrlRegex.test(webhookUrl)) {
+    throw new Error('올바른 Discord Webhook URL이 아닙니다.');
+  }
 }
